93085510096cd2541460d2a6fb0a48e7b2840353
dabibu153 <68466028+dabibu153@users.noreply.github.com>
2020-08-11 21:23:37 +0530

    second commit

--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -3,32 +3,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function ProductDetails(props) {
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
     const getData = async () => {
       let data = await axios.get(
         `http://localhost:5000/api/products/${props.match.params.id}`
       );
-      console.log(data);
-      //   setProduct(data.data);
+      console.log(data.data);
+      setProduct(data.data);
     };
     getData();
   }, []);
   return (
     <div>
-      {/* {product.map((cpu) => {
-        if (cpu._id === parseInt(props.match.params.id)) {
-          <div className="product-container">
-            <h3>{cpu.name}</h3>
-            <h5>{cpu.brand}</h5>
-            <ul>
-              <li className="product-li">Price: {cpu.price}</li>
-              <li className="product-li">generation: {cpu.generation}</li>
-            </ul>
-          </div>;
-        }
-      })} */}
+      <div className="one-product-container">
+        <h3>{product.name}</h3>
+        <h5>{product.brand}</h5>
+        <ul>
+          <li className="one-product-li">Cores: {product.cores}</li>
+          <li className="one-product-li">Threads: {product.threads}</li>
+          <li className="one-product-li">Base clock: {product.baseClock}</li>
+          <li className="one-product-li">Boost clock: {product.turboClock}</li>
+          <li className="one-product-li">Price: {product.price}</li>
+          <li className="one-product-li">Generation: {product.generation}</li>
+        </ul>
+      </div>
+      ;
     </div>
   );
 }
