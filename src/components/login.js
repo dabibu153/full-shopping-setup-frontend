e93fa4b53180c4a7f907bee1e53b0ceb126ec888
dabibu153 <68466028+dabibu153@users.noreply.github.com>
2020-08-13 20:45:54 +0530

    commit late 8/13/2020

--- src/components/login.js
+++ src/components/login.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function login() {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    let data = {
+      email: email,
+      password: pass,
+    };
+    axios.post("http://localhost:5000/api/users/login", data).then((res) => {
+      setResult(res.data);
+    });
+  };
+
   return (
     <div>
       <form>
         <label>email:</label>
-        <input type="email"></input>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
         <br />
         <label>password</label>
-        <input type="password"></input>
+        <input
+          type="password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        ></input>
         <br />
-        <button type="submit">login</button>
+        <button type="submit" onClick={(e) => handleLogin(e)}>
+          login
+        </button>
       </form>
 
       <Link to="/register">
         <h3>WOULD U LIKE TO REGISTER</h3>
       </Link>
+
+      <h3>{result}</h3>
     </div>
   );
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const data = {
+      userName: userName,
+      email: email,
+      password: pass,
+    };
+    const res = await axios.post(
+      "http://localhost:5000/api/users/register",
+      data
+    );
+    console.log(res.data);
+    setResult(res.data);
+  };
   return (
     <div>
       <form>
         <label>user name</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        ></input>
         <br />
         <label>email</label>
-        <input type="email"></input>
-        <br />
-        <label>mobile</label>
-        <input type="number"></input>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
         <br />
         <label>password</label>
-        <input type="password"></input>
+        <input
+          type="text"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        ></input>
         <br />
-        <button type="submit">register</button>
+        <button onClick={(e) => handleRegister(e)}>register</button>
       </form>
+
+      <h3>{result}</h3>
     </div>
   );
 }
